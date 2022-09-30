78cb04057270a5ab56dbfb4618d35657c73d4489
Luboso <107425845+Luboso@users.noreply.github.com>
2022-09-30 20:18:00 +0200

    Add files via upload

---
+++ nightfury/video.php
@@ -0,0 +1,73 @@
+<?php include 'shares/header.php'; ?>
+	<main>
+		<article>
+			<header class="post-header">
+				<div class="container">
+					<h1 class="post-title">Portfólio našich prác</h1>
+				</div>
+			</header>
+			<div class="post-content">
+				<div class="container">
+
+					<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
+
+					<ul class="menu controls">
+						<li data-from="left"><a href="index.php">Všetko</a></li>
+						<li data-from="right"><a href="web.php">Web</a></li>
+						<li data-from="left"><a href="branding.php">Branding</a></li>
+						<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
+						<li data-from="left" class="selected"><a href="#video">Video</a></li>
+					</ul>
+
+				</div>
+
+				<section class="gallery">
+					<div class="gallery-set container" id="video">
+						<a href="assets/img/zim/image-1.jpg">
+							<img src="assets/img/zim/thumb-1.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-2.jpg">
+							<img src="assets/img/zim/thumb-2.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-3.jpg">
+							<img src="assets/img/zim/thumb-3.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-4.jpg">
+							<img src="assets/img/zim/thumb-4.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-5.jpg">
+							<img src="assets/img/zim/thumb-5.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-6.jpg">
+							<img src="assets/img/zim/thumb-6.jpg" height="192" width="383" alt="space invader">
+						</a>
+						<a href="assets/img/zim/image-7.jpg">
+							<img src="assets/img/zim/thumb-7.jpg" height="192" width="383" alt="space invader">
+						</a>
+					</div>
+				</section>
+			</div>
+		</article>
+	</main>
+
+	<aside class="pre-footer">
+		<div class="container">
+			<p>
+				<strong>Zaujali sme Vás pre nový projekt?</strong>
+				Jednoducho nám zavolajte alebo napíšte.
+			</p>
+
+			<ul class="menu personal">
+				<li><i class="fa fa-phone"></i><a href="tel:+421900111222">+421 900 111 222</a></li>
+				<li><i class="fa fa-envelope"></i><a href="mailto:email@mailinator.com">email@mailinator.com</a></li>
+			</ul>
+		</div>
+	</aside>
+
+	<?php include 'shares/footer.php' ?>
+
+	<!-- scripts -->
+	<script src="js/lightbox.js"></script>
+
+</body>
+</html>
